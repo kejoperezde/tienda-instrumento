b221876e21adb5b6f42e203aa016a119dc8602b0
25carlosalberto <scorpionmkx25@gmail.com>
2022-05-26 11:19:28 -0600

    Cambios y acomodo

---
+++ Procesar/ProcesarInstrumentos.java
@@ -0,0 +1,38 @@
+package Procesar;
+import Models.*;
+import Exception.*;
+public class ProcesarInstrumentos {
+    public void procesarIns(Instrumento[] i,Registrobin rb,Recibocsv rc) throws ExcCantidad,ExcDescuento,ExcPrecio{
+        for (Instrumento ref : i) {
+            try {
+                if(ref.getCantidad()<0){
+                    throw new ExcCantidad();
+                }
+                else{
+                    if(ref.getPrecio()<100){
+                        throw new ExcPrecio();
+                    }
+                    if(ref.getDesc()!=0){
+                        System.out.println(ref.calcularPrecio(ref.getDesc()));
+                    }
+                    else{
+                        System.out.println(ref.calcularPrecio());
+                    } 
+                    rb.agregarInstrumentobin(ref);
+                    rc.agregarInstrumento(ref);
+                }
+            } catch (ExcCantidad e) {
+                System.out.println("Error tipo "+e.getClass().getSimpleName());
+                //e.StackOverflowError();
+            } 
+            catch (ExcDescuento e) {
+                System.out.println("Error tipo "+e.getClass().getSimpleName());
+                //e.StackOverflowError();
+            }  
+            catch (ExcPrecio e) {
+                System.out.println("Error tipo "+e.getClass().getSimpleName());
+                //e.StackOverflowError();
+            }  
+        }
+    }
+}
