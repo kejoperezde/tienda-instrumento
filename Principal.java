ba473ab862c5c4542a985348587ce41d1eb9204f
Carlos Alberto Ruiz Guitron <scorpionmkx25@gmail.com>
2022-05-25 21:09:55 -0600

    Edicioncarlos1

--- Principal.java
+++ Principal.java
@@ -4,7 +4,7 @@ public class Principal {
     public static void main(String args[]){
         ProcesarInstrumentos pi= new ProcesarInstrumentos();
         Instrumento i[] = new Instrumento[4];
-        i[0]=new Violin("Stradivarius","4/4",10000,1,"Yamaha",true,.5);
+        i[0]=new Violin("Stradivarius","4/4",20,1,"Yamaha",true,.5);
         i[1]=new Violin("Stradivarius","4/4",10000,1,"Yamaha",0);
         i[2]=new Guitarra("Metal", "Alto y Ancho", 3000, -12, "Yamaha",0);
         i[3]=new Guitarra("Country", "Bajo y Ancho", 100, 2, "Yamaha", true,.4);
